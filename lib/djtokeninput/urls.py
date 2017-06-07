2871f106e81448f8c52f787a4a6033cf7ced6945
Kanyin Olugbake <kolugbake@hotmail.com>
2017-06-07 22:55:27 +0100

    removed patterns from urls.py

--- lib/djtokeninput/urls.py
+++ lib/djtokeninput/urls.py
@@ -3,6 +3,6 @@
 from django.conf.urls import url, include
 
 
-urlpatterns = patterns("",
+urlpatterns = [
   url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", "djtokeninput.views.search", name="djtokeninput_search")
-)
+]
