4674ed01953b5897283d5482327ca5679f89b6e7
Kanyin Olugbake <kolugbake@hotmail.com>
2017-06-07 23:00:40 +0100

    updated the urlpatterns due to dj 1.10 update
    update no longer allows views as strings

--- lib/djtokeninput/urls.py
+++ lib/djtokeninput/urls.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 from django.conf.urls import url, include
+from .views import search
 
 
 urlpatterns = [
-  url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", "djtokeninput.views.search", name="djtokeninput_search")
+  url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", search, name="djtokeninput_search")
 ]
