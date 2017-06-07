84566805eec1856aeeed08dabf5ff1affb0e4404
Kanyin Olugbake <kolugbake@hotmail.com>
2017-06-07 23:49:11 +0100

    converted map to list object for py3+
    map() in Python 3 is a generator function, which is not serializeable in JSON. You can make it serializeable by converting it to a list

--- lib/djtokeninput/views.py
+++ lib/djtokeninput/views.py
@@ -7,9 +7,9 @@ from django.shortcuts import get_object_or_404
 
 
 def _tokens(query_set, keys=("id", "name")):
-  return map(
+  return list(map(
     lambda v: dict(zip(keys, v)),
-    query_set.values_list(*keys))
+    query_set.values_list(*keys)))
 
 def search(req, app_label, model):
   content_type = get_object_or_404(ContentType, app_label=app_label, model=model)
@@ -19,7 +19,6 @@ def search(req, app_label, model):
     if "q" in req.GET:
       query_set = model.search(req.GET["q"])
       tokens = _tokens(query_set)
-
     else:
       tokens = []
 
